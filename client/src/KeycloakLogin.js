2ef0fc5b3a8d671c91bdf49c104c4d12810483e3
Libello <liebl.marc@gmail.com>
2019-11-07 20:31:26 +0100

    Keycloak-Login erweiert

--- client/src/KeycloakLogin.js
+++ client/src/KeycloakLogin.js
@@ -1,13 +1,24 @@
 import React, {Component} from 'react'
-import Keycloak from "keycloak-js";
+import Keycloak from "keycloak-js"
+
+const Config = {
+    url: 'http://libellosnas:8088/auth', 
+    realm: 'web-clients', 
+    clientId: 'react-client', 
+    onLoad: 'login-required'
+}
 
 class KeycloakLogin extends Component {
 
     componentDidMount() {
-        const keycloak = Keycloak('/keycloak.json');
+        const keycloak = Keycloak(Config);
+        keycloak.init({onLoad: "login-required", promiseType: 'native'}).then(authentication => {
+            console.log(authentication);
+        });
     }
 
     render() {
+    
         return <div>Ready to initialize</div>
     }
 
